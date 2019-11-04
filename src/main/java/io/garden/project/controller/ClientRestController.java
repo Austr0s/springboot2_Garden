8a9ba266f43ff71c4c348e4fe7aeb7ae187375f9
Austr0s <aosloco@gmail.com>
2019-11-04 05:35:15 +0000

    SWAGGER2 CONFIG
    Configuration Swagger2 to see functionality of Application.
    This implementation is use to Document all Application to use  and see
    documentation about entities, requests and responses.

--- src/main/java/io/garden/project/controller/ClientRestController.java
+++ src/main/java/io/garden/project/controller/ClientRestController.java
@@ -11,15 +11,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 import io.garden.project.model.entity.Client;
 import io.garden.project.service.ClientService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
-@RequestMapping("/api/client")
+@RequestMapping("/api/v1/client")
 public class ClientRestController {
 
 	@Autowired
 	private ClientService service;
 	
 	@GetMapping("/{id}")
+	@ApiOperation(value = "Find client by Id", notes = "Provide an id  to look up specific Client from Api", response = Client.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved Client"),
+			@ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+			@ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+			@ApiResponse(code = 404, message = "The resource  was not found")
+	})	
 	public ResponseEntity<Optional<Client>> getOne(@PathVariable(value = "id") Long id) {
 		
 		Optional<Client> client = service.findOneById(id);
@@ -28,6 +38,13 @@ public class ClientRestController {
 	}
 	
 	@GetMapping
+	@ApiOperation(value = "Find all Clients", notes = "Returns all Clients from Api", response = Client.class)
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Successfully retrieved Clients"),
+			@ApiResponse(code = 401, message = "The request has not been applied because it lacks valid authentication credentials for the target resource"),
+			@ApiResponse(code = 403, message = "The server understood the request but refuses to authorize it"),
+			@ApiResponse(code = 404, message = "The resource  was not found")
+	})
 	public ResponseEntity<Iterable<Client>> getAll(){
 		Iterable<Client> clients = service.findAll();
 		
--- src/main/java/io/garden/project/repository/ClientRepository.java
+++ src/main/java/io/garden/project/repository/ClientRepository.java
@@ -1,9 +1,11 @@
 package io.garden.project.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import io.garden.project.model.entity.Client;
 
+@Repository
 public interface ClientRepository extends CrudRepository<Client, Long>{
 
 }
